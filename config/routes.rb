70d7cadbc1e7868a8a16afe3e0b9d04d3ada9d29
mmckay999 <36682659+mmckay999@users.noreply.github.com>
2019-06-29 12:49:10 -0400

    Routes adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   resources :comics
-
+  root 'comics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
